60319935e27f0aca38b6c910cb80337012ce2b1c
Marco Principe <131398860+princh13@users.noreply.github.com>
2023-05-11 16:01:32 -0400

    Update

--- nfl_dashboard2.py
+++ nfl_dashboard2.py
@@ -7,7 +7,7 @@ st.title('NFL Football Stats (Rushing) Explorer')
 
 st.markdown("""
 This app performs simple webscraping of NFL Football player stats data (focusing on Rushing)!
-* **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn
+* **Python libraries:** base64, pandas, streamlit
 * **Data source:** [pro-football-reference.com](https://www.pro-football-reference.com/).
 """)
 
@@ -16,7 +16,7 @@ selected_year = st.sidebar.selectbox('Year', list(reversed(range(1990,2023))))
 
 # Web scraping of NFL player stats
 # https://www.pro-football-reference.com/years/2019/rushing.htm
-@st.cache
+@st.cache_data
 def load_data(year):
     url = "https://www.pro-football-reference.com/years/" + str(year) + "/rushing.htm"
     html = pd.read_html(url, header = 1)
